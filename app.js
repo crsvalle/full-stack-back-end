2cbea7d9c9e125b512d23d1fcddcad3e39c1e3a4
crsvalle <cristian.valle@pursuit.org>
2023-08-16 23:53:18 -0400

    updated app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+const cors = require("cors");
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("The Blog API");
+});
+
+// Snacks ROUTES
+const blogController = require("./controllers/blogController.js");
+app.use("/blogs", blogController);
+
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// EXPORT
+module.exports = app;
